2fa2ea243a8f4be1c38d9edc3397819f9b76b1df
thommasm <th_martinez@javeriana.edu.co>
2026-05-31 13:04:15 -0500

    Add files via upload

---
+++ PROYECTO_2_PROGRAAVANZADA/src/main/java/com/puj/App.java
@@ -0,0 +1,13 @@
+package com.puj;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
